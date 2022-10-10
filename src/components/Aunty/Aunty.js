dde53b1b649af6e5f840471fbbc5acb1ee14d7c6
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-10 17:14:10 +0600

    update

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Cousin from "../Cousin/Cousin";
 
 const Aunty = () => {
   return (
     <div>
       <h1>Aunty</h1>
+      <section className="flex">
+        <Cousin></Cousin>
+        <Cousin></Cousin>
+      </section>
     </div>
   );
 };
