fff790210525258d2320835e82e6f48359d6626d
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-10 17:29:41 +0600

    update

--- src/components/MySelf/MySelf.js
+++ src/components/MySelf/MySelf.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Special from "../Special/Special";
 
 const MySelf = ({ house }) => {
   return (
     <div>
       <h1>MySelf</h1>
       <p>House : {house}</p>
+      <section className="flex">
+        <Special></Special>
+      </section>
     </div>
   );
 };
---
+++ src/components/Special/Special.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Special = () => {
+  return (
+    <div>
+      <h1>Special</h1>
+    </div>
+  );
+};
+
+export default Special;
