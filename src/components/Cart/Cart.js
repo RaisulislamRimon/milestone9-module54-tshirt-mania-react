d73798dc444ea2e4f9ef8863db494f195b5fdfa9
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-10 15:07:02 +0600

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,9 @@ const Cart = ({ cart }) => {
     <div>
       <h2>Order Summary</h2>
       <h5>Order Quantity : {cart.length}</h5>
+      {cart.map((tshirt) => (
+        <p key={tshirt._id}>{tshirt.name}</p>
+      ))}
     </div>
   );
 };
